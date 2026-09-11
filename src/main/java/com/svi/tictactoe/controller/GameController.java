902d8791b3a8e5595d8279c4f1e3d350ba9dd305
Giovanni M. Leo <giovannileo100@gmail.com>
2026-09-11 10:09:16 +0800

    chore: updated get game removed /status

--- src/main/java/com/svi/tictactoe/controller/GameController.java
+++ src/main/java/com/svi/tictactoe/controller/GameController.java
@@ -72,7 +72,7 @@ public class GameController {
         return ResponseEntity.ok(new JoinGameResponse(message, participant));
     }
 
-    @GetMapping("/{roomCode}/status")
+    @GetMapping("/{roomCode}")
     public ResponseEntity<GameStatusResponse> checkGameStatus(@PathVariable String roomCode) {
         Game game = gameService.getGame(roomCode);
 
